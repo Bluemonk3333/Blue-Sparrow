184bfeb164325d673879a3ceb4177f1434bdc197
Blue Monk <bluemonk@gmail.com>
2024-07-09 17:18:09 +0530

    Add everything

---
+++ main.js
@@ -0,0 +1,12 @@
+// const dLink = document.getElementById("download_link");
+
+// downloadPopUp = (event) => {
+//   askUser = confirm("Do you want to download!");
+//   if (askUser === true) {
+//     alert("Thank you for downloading!");
+//   } else {
+//     alert("Operation aborted! file is not downloading");
+//   }
+// };
+
+// dLink.addEventListener("click", downloadPopUp);
